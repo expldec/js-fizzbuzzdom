b018151428f8ea7a7e2b42dccb47959108d7a86e
expldec <explosive.deconstruction@gmail.com>
2022-04-11 14:06:39 +0200

    added DOM Manipulation to FizzBuzz logic

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-for (let i = 0; i <= 100; i++) {
+row = document.querySelector(".row");
+
+for (let i = 1; i <= 100; i++) {
+    let appendMe = document.createElement("div");
+    appendMe.classList.add('box');
+    console.log(appendMe);
+
     if (i % 15 === 0) {
+        appendMe.classList.add('fizzbuzz');
+        appendMe.innerText = "Fizz Buzz!!";
         console.log("FizzBuzz!!");
     }
     else if (i % 5 === 0) {
+        appendMe.classList.add('buzz');
+        appendMe.innerText = "Buzz!";
         console.log("Buzz!");
     }
     else if (i % 3 === 0) {
+        appendMe.classList.add('fizz');
+        appendMe.innerText = "Fizz!";
         console.log("Fizz!");
     }
     else {
         console.log(i);
+        appendMe.innerText = `${i}`;
     }
+
+    
+    row.append(appendMe);
 }
